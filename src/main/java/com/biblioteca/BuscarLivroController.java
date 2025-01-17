e9b54c3f7cf2d73b5fa6b54160a218378d8967d0
FabricioS0 <fsoliveira899@gmail.com>
2025-01-16 21:13:56 -0300

    update biblioteca

---
+++ src/main/java/com/biblioteca/BuscarLivroController.java
@@ -0,0 +1,22 @@
+package com.biblioteca;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class BuscarLivroController {
+
+    @FXML
+    private TextField Livro;
+
+    @FXML
+    void BuscarLivro(ActionEvent event) {
+
+    }
+
+    @FXML
+    void RetornaHome(ActionEvent event) {
+
+    }
+
+}
