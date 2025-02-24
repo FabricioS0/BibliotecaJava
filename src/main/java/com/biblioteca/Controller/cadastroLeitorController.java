d75a929207898f6b5883e69504bbc96f7ce965bb
FabricioS0 <fsoliveira899@gmail.com>
2025-02-24 14:29:45 -0300

    Cadastro de leitor

---
+++ src/main/java/com/biblioteca/Controller/cadastroLeitorController.java
@@ -0,0 +1,12 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class cadastroLeitorController {
+
+    @FXML
+    private TextField matriculaLeitor;
+
+    @FXML
+    private TextField nomeLeitor;
+
+}
